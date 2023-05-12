e2b493c2fee62a2827629719d2bb0ecdd5b92406
franSborges <borgesfran988@gmail.com>
2023-05-11 21:29:10 -0300

    :feat: Game seed created

--- src/main/java/com/frandev/dslist/entities/Game.java
+++ src/main/java/com/frandev/dslist/entities/Game.java
@@ -18,26 +18,30 @@ public class Game {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String title;
-  
+
   @Column(name = "game_year")
   private Integer year;
   private String genre;
-  private String platform;
+  private String platforms;
   private Double score;
   private String imgUrl;
+  
+  @Column(columnDefinition = "TEXT")
   private String shortDescription;
+  
+  @Column(columnDefinition = "TEXT")
   private String longDescription;
 
   public Game() {
   }
 
-  public Game(Long id, String title, Integer year, String genre, String platform, Double score, String imgUrl,
+  public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
       String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
         this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
@@ -78,11 +82,11 @@ public class Game {
     }
 
     public String getPlatform() {
-        return platform;
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatform(String platforms) {
+        this.platforms = platforms;
     }
 
     public Double getScore() {
